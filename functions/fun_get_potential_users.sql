ae8e5818145a1780afa78a4a0926178d28363cf9
Juan Caballero <juandacf@gmail.com>
2025-12-05 21:15:40 -0500

    add age to potential matches service

--- functions/fun_get_potential_users.sql
+++ functions/fun_get_potential_users.sql
@@ -7,7 +7,8 @@ RETURNS TABLE (
     native_lang_id_out VARCHAR,
     target_lang_id_out VARCHAR,
     description TEXT,
-    profile_photo VARCHAR
+    profile_photo VARCHAR,
+    age INTEGER
 )
 AS $$
 DECLARE
@@ -21,9 +22,9 @@ BEGIN
     FROM users
     WHERE users.id_user = p_id_user;
 
-IF v_limit = 0 THEN
-    v_limit := 50; -- o cualquier número por defecto
-END IF;
+    IF v_limit = 0 THEN
+        v_limit := 50;
+    END IF;
 
     RETURN QUERY
     SELECT 
@@ -34,32 +35,27 @@ END IF;
         u2.native_lang_id AS native_lang_id_out,
         u2.target_lang_id AS target_lang_id_out,
         u2.description,
-        u2.profile_photo
+        u2.profile_photo,
+        EXTRACT(YEAR FROM age(CURRENT_DATE, u2.birth_date)) AS age
     FROM users u2
     WHERE 
-        -- Matching de lenguas
         u2.native_lang_id = v_user_target
         AND u2.target_lang_id = v_user_native
         AND u2.id_user <> p_id_user
         AND u2.is_active = TRUE
         AND u2.role_code = 'user'
-
-        -- NO mostrar usuarios a los que YA les di like
         AND NOT EXISTS (
             SELECT 1
             FROM user_likes ul
             WHERE ul.id_user_giver = p_id_user
               AND ul.id_user_receiver = u2.id_user
         )
-
-        -- NO mostrar usuarios con los que YA tengo match
         AND NOT EXISTS (
             SELECT 1
             FROM user_matches m
             WHERE (m.user_1 = p_id_user AND m.user_2 = u2.id_user)
                OR (m.user_2 = p_id_user AND m.user_1 = u2.id_user)
         )
-
     ORDER BY u2.last_login DESC
     LIMIT v_limit;
 END;
